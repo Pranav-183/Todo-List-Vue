1ea849d5c62646d9e3d34cb0831c4d5e2287a1d9
Pranav <brindhameera@gmail.com>
2022-10-17 21:27:59 +0530

    changed config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: 'pranav-183.github.io/Todo-List-Vue/'
+  publicPath: '/Todo-List-Vue'
 })
